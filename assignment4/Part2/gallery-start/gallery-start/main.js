8acfc8d6155a4331cc9edf7defac3fd3abc22df0
soumik-debnath <soumik.debnath@dcmail.ca>
2024-04-05 23:03:42 -0400

    add alttext

--- assignment4/Part2/gallery-start/gallery-start/main.js
+++ assignment4/Part2/gallery-start/gallery-start/main.js
@@ -8,7 +8,7 @@ const overlay = document.querySelector('.overlay');
 const imageArray = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"] ;
 
 /* Declaring the alternative text for each image file */
-const  =["eye", "sea wave", " flower", "art", "butterfly"]
+const alttext =["eye", "sea wave", " flower", "art", "butterfly"]
 
 /* Looping through images */
 for (let i = 0; i< imageArray.length; i++) {
