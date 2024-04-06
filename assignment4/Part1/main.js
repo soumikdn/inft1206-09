b86e894d07e5010082af19e3f7fb7dda4d0e0fba
soumik-debnath <soumik.debnath@dcmail.ca>
2024-04-05 21:14:53 -0400

    insertx

--- assignment4/Part1/main.js
+++ assignment4/Part1/main.js
@@ -6,23 +6,23 @@ function randomValueFromArray(array){
   const random = Math.floor(Math.random()*array.length);
   return array[random];
 }
-const storyText="It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
-const insertX= ["Willy the Goblin", "Big Daddy", "Father Christmas"];
-const insertY= ["the soup kitchen", "Disneylandthe", "White House"];
-const insertZ= ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"];
+ storyText="It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
+ insertX= ["Willy the Goblin", "Big Daddy", "Father Christmas"];
+ insertY= ["the soup kitchen", "Disneylandthe", "White House"];
+ insertZ= ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"];
 
 randomize.addEventListener('click', result);
 
 function result() {
     let newstory = storyText;
     
-    const xitem = randomValueFromArray(insertX);
-    const yitem = randomValueFromArray(insertY);
-    const zitem = randomValueFromArray(insertZ);
+    let xitem = randomValueFromArray(insertX);
+    let yitem = randomValueFromArray(insertY);
+    let zitem = randomValueFromArray(insertZ);
 
-    newstory = newstory.replaceAll(":insertx:", xitem);
-    newstory = newstory.replaceAll(":inserty:", yitem);
-    newstory = newstory.replaceAll(":insertz:", zitem);
+    newstory = newstory.replace(":insertx:", xitem);
+    newstory = newstory.replace(":inserty:", yitem);
+    newstory = newstory.replace(":insertz:", zitem);
 
   if(customName.value !== '') {
     const name = customName.value;
@@ -31,9 +31,9 @@ function result() {
   }
 
   if(document.getElementById("uk").checked) {
-    const weight = `${Math.round(300/ 14)} stone`;
-    const temperature = `${Math.round(94-32) *(5/9)} centigrade` ;
-    newstory = newstory.replace("94 fahrenheit", temperature)
+    const weight = Math.round(300 * 0.0714 ) + "stone" ;
+    const temperature = Math.round((94-32) * (5/9)) + "centigrade" ;
+    newstory = newstory.replace("94 fahrenheit", temperature);
     newstory = newstory.replace("300 pounds", weight )
 
   }
